e9ebfade522b62359dff7597c399d8cb771150d2
Danila Marychev <marychtv5@gmail.com>
2021-10-01 19:02:01 +0300

    Урок с операторами и операндами

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Учеба";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 21;
+let rollback = 50;
+let fullPrice = 100000;
+let adaptive = true;
 
-alert("Спасибо");
-console.log("Вам");
+console.log(title, fullPrice, adaptive);
+console.log(screens.length);
+console.log(screenPrice + "$");
+console.log(fullPrice + "₽");
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
