d8fcd9e2910c96a60e2448f2be357a3623b30806
Danila Marychev <marychtv5@gmail.com>
2021-10-07 00:56:18 +0300

    Задание с функциями

--- script.js
+++ script.js
@@ -1,46 +1,72 @@
 'use strict';
-let title = "Учеба";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 21;
-let rollback = 50;
-let fullPrice = 100000;
-let adaptive = true;
 
-console.log(title, fullPrice, adaptive);
-console.log(screens.length);
-console.log(screenPrice + "$");
-console.log(fullPrice + "₽");
-console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100));
-
-title = prompt('Как называется ваш проект?');
-console.log(title);
-screens = prompt('Какие типы экранов нужно разрабатывать?');
-console.log(screens);
-screenPrice = +prompt('Сколько будет стоить данная работа?');
-console.log(screenPrice);
-adaptive = confirm('Нужен ли адаптив на сайте?');
-console.log(adaptive);
-
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разрабатывать?');
+let screenPrice = +prompt('Сколько будет стоить данная работа?');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
-
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-console.log(fullPrice);
-
+let rollback = 50;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = Math.ceil(fullPrice - rollback / 100);
-console.log(servicePercentPrice);
+let allServicePrices;
 
-if (fullPrice > 30000) {
-    console.log("Даем скидку в 10%");
-} else if (fullPrice > 15000 && fullPrice < 30000) {
-    console.log("Даем скидку в 5%");
-} else if (fullPrice < 15000 && fullPrice > 0) {
-    console.log("Скидка не предусмотрена");
-} else if (fullPrice < 0) {
-    console.log("Что то пошло не так");
-} else if (fullPrice == 0 || fullPrice || 15000 || fullPrice == 30000) {
-    console.log("Даем скидку в 5%");
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
 }
+
+const getAllServicePrices = function () {
+    allServicePrices = servicePrice1 + servicePrice2;
+}
+
+function getFullPrice() {
+    fullPrice = screenPrice + allServicePrices;
+    return fullPrice;
+
+}
+
+const getTitle = function (title) {
+    title = title.trim();
+    title = title.toLowerCase();
+    title = title[0].toUpperCase() + title.slice(1);
+    return title;
+}
+
+function getServicePercentPrices() {
+    servicePercentPrice = Math.ceil(fullPrice - rollback / 100);
+    return servicePercentPrice;
+}
+
+const getRollbackMessage = function (price) {
+
+    if (price > 30000) {
+        return "Даем скидку в 10%";
+    } else if (price > 15000 && fullPrice < 30000) {
+        return "Даем скидку в 5%";
+    } else if (price < 15000 && fullPrice > 0) {
+        return "Скидка не предусмотрена";
+    } else if (price < 0) {
+        return "Что то пошло не так";
+    } else if (price == 0 || price == 15000 || price == 30000) {
+        return "Даем скидку в 5%";
+    }
+}
+
+
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+
+
+console.log(screens.split());
+console.log(getRollbackMessage(fullPrice));
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+console.log(screen.length);
+console.log(servicePercentPrice);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
